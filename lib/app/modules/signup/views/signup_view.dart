41caf10bc2a4756f02c6783917b8e95d5c53bed1
Zain Naqvi <Zain.N@devnatives.com>
2022-12-06 22:44:56 +0500

    Sign up text filed is added

--- lib/app/modules/signup/views/signup_view.dart
+++ lib/app/modules/signup/views/signup_view.dart
@@ -25,14 +25,70 @@ class SignupView extends GetView<SignupController> {
                 topRight: Radius.circular(12.r),
                 bottomRight: Radius.circular(12.r),
               ),
-              color: AppColors.WHITE_COLOR,
+              color: Colors.blue.withOpacity(0.2),
             ),
             child: Row(
               children: [
                 CircleAvatar(
                   radius: 28.r,
-                  backgroundColor: Colors.teal,
+                  backgroundColor: Colors.white,
+                  child: Center(
+                    child: Icon(
+                      Icons.person_outline,
+                    ),
+                  ),
                 ),
+                Expanded(
+                    child: TextFormField(
+                  textAlign: TextAlign.start,
+                  onEditingComplete: () {},
+                  focusNode: null,
+                  autofocus: false,
+                  readOnly: false,
+                  keyboardType: null,
+                  obscureText: false,
+                  textInputAction: null,
+                  controller: null,
+                  validator: null,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  cursorColor: Colors.white,
+                  // style: TextStyle(
+                  //   color: filled
+                  //       ? AppColor.PRIMARY_WHITE_TEXT_COLOR
+                  //       : AppColor.PRIMARY_SCAFFOLD_COLOR,
+                  //   height: 1.3.r,
+                  // ),
+                  decoration: InputDecoration(
+                    contentPadding:
+                        EdgeInsets.only(bottom: 0.0, top: 15.0.r, left: 8.w),
+                    errorMaxLines: 2,
+                    errorStyle: TextStyle(color: Colors.red),
+                    errorBorder: const OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Colors.transparent, width: 0.0),
+                    ),
+                    focusedErrorBorder: const OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Colors.transparent, width: 0.0),
+                    ),
+                    hintText: 'Enter email',
+                    hintStyle: TextStyle(
+                      color: Colors.white,
+                    ),
+                    enabledBorder: outlineBorder(),
+                    focusedBorder: outlineBorder(),
+                    border: outlineBorder(),
+                  ),
+                )),
+                // isSuffixIcon
+                //     ? IconButton(
+                //         onPressed: onPressed,
+                //         icon: Icon(
+                //           suffixIcon,
+                //           color: AppColor.PRIMARY_COLOR,
+                //         ),
+                //       )
+                //     : const SizedBox(),
               ],
             ),
           ),
@@ -40,4 +96,11 @@ class SignupView extends GetView<SignupController> {
       ),
     );
   }
+}
+
+OutlineInputBorder outlineBorder() {
+  return OutlineInputBorder(
+    borderRadius: BorderRadius.circular(8.r),
+    borderSide: const BorderSide(color: Colors.transparent),
+  );
 }
