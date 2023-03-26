df07a90b0de4be46383d37dff41f667d68f230f1
zainnaqvi <syedzainnaqvi3324@gmail.com>
2023-03-27 03:18:34 +0500

    overflow issues solved

--- lib/app/modules/home/widgets/appbar.dart
+++ lib/app/modules/home/widgets/appbar.dart
@@ -1,9 +1,8 @@
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:open_close_loop_recycling/app/controller/home_controller.dart';
 import 'package:open_close_loop_recycling/app/modules/notification/notification.dart';
-
 
 import '../../../model/notification_model.dart';
 import '../../../utils/colors.dart';
@@ -13,43 +12,44 @@ AppBar appbar(final controller) {
     elevation: 0,
     backgroundColor: AppColors.PRIMARY_COLOR,
     actions: [
-     StreamBuilder<List<NotificationModel>>(
-          stream: controller.getUnreadNotifications().asStream(),
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) {
-              return const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: CircularProgressIndicator(
-                  strokeWidth: 1,
-                  color: Colors.white,
-                ),
-              );
-            }
-
+      StreamBuilder<List<NotificationModel>>(
+        stream: controller.getUnreadNotifications().asStream(),
+        builder: (context, snapshot) {
+if (snapshot.connectionState == ConnectionState.waiting) {
+            // Show spinner while data is being fetched
+            return const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: CircularProgressIndicator(
+                strokeWidth: 1,
+                color: Colors.white,
+              ),
+            );
+          } else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+            // Show bell icon with notification count
             List<NotificationModel> notifications = snapshot.data!;
             return IconButton(
-              icon: Stack(clipBehavior: Clip.none,
+              icon: Stack(
+                clipBehavior: Clip.none,
                 children: [
                   const Icon(Icons.notifications_active_outlined),
-                  if (notifications.isNotEmpty)
-                    Positioned(
-                      top: -6.h,
-                      right: -4.w,
-                      child: Container(
-                        padding: EdgeInsets.all(4.r),
-                        decoration: const BoxDecoration(
-                          color: Colors.red,shape: BoxShape.circle,
-                       
-                        ),
-                        child: Text(
-                          notifications.length.toString(),
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 12,
-                          ),
+                  Positioned(
+                    top: -6.h,
+                    right: -4.w,
+                    child: Container(
+                      padding: EdgeInsets.all(4.r),
+                      decoration: const BoxDecoration(
+                        color: Colors.red,
+                        shape: BoxShape.circle,
+                      ),
+                      child: Text(
+                        notifications.length.toString(),
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
                         ),
                       ),
                     ),
+                  ),
                 ],
               ),
               onPressed: () {
@@ -57,8 +57,35 @@ AppBar appbar(final controller) {
                 Get.to(NotificationView());
               },
             );
-          },
-        ),
+          } else {
+            // Show bell icon with no notification count
+            return IconButton(
+              icon: Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  const Icon(Icons.notifications_active_outlined),
+                  Positioned(
+                    top: -6.h,
+                    right: -6.w,
+                    child: Container(
+                      padding: EdgeInsets.all(2.r),
+                      decoration: const BoxDecoration(
+                        color: Colors.red,
+                        shape: BoxShape.circle,
+                      ),
+                      child: Badge()
+                    ),
+                  ),
+                ],
+              ),
+              onPressed: () {
+                //  await controller.markNotificationAsRead(FirebaseAuth.instance.currentUser!.uid);
+                Get.to(NotificationView());
+              },
+            );
+          }
+        },
+      ),
     ],
   );
 }
